d55be95eb2adb2cc95a5a16eaa355a52ce3de983
Joanne <jungyoon.lim99@gmail.com>
2023-09-21 23:44:14 -0700

    dashboard update

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -2,22 +2,30 @@ import React from 'react';
 import Layout from '../src/app/components/Layout';
 import FlashcardApp from '../src/app/components/FlashcardApp';
 import PaymentForm from '../src/app/components/PaymentForm';
+import { Button, Card, LinearProgress, Typography } from '@material-ui/core';
+import { InfoOutlined, StarBorder } from '@material-ui/icons';
 
 const Dashboard = () => {
   return (
     <Layout>
-      <div className="container mx-auto px-6 py-3">
-        <h1 className="text-3xl font-bold text-gray-800 dark:text-white">Dashboard</h1>
-        <div className="mt-6">
+      <div className="container mx-auto p-6">
+        <Typography variant="h4" component="h1" gutterBottom>Welcome to Your Dashboard</Typography>
+
+        {/* Flashcard Section */}
+        <Card className="p-6 mb-6 transition-shadow hover:shadow-lg">
+          <Typography variant="h5" component="h2" gutterBottom>Interactive Flashcards <InfoOutlined /></Typography>
+          <Typography paragraph>Enhance your skills with our interactive flashcards. Track your progress below.</Typography>
           <FlashcardApp />
-        </div>
-        <div className="mt-6">
-          <h2 className="text-2xl font-bold text-gray-800 dark:text-white">Upgrade to Premium</h2>
-          <p className="mt-2 text-gray-600 dark:text-gray-400">Unlock unlimited flashcards and advanced features</p>
-          <div className="mt-4">
-            <PaymentForm />
-          </div>
-        </div>
+          <LinearProgress value={50} variant="determinate" className="mt-4" />
+        </Card>
+
+        {/* Upgrade Section */}
+        <Card className="p-6 transition-shadow hover:shadow-lg">
+          <Typography variant="h5" component="h2" gutterBottom>Go Premium <StarBorder /></Typography>
+          <Typography paragraph>Unlock unlimited access to flashcards and advanced features. Upgrade today.</Typography>
+          <PaymentForm />
+          <Button variant="contained" color="primary" className="mt-4" disableElevation>Upgrade Now</Button>
+        </Card>
       </div>
     </Layout>
   );
